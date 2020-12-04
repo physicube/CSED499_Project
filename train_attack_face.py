6ea24ccb90416ce721a391d2bcce38bd60de9b76
Sangwoo Ji <sangwooji@postech.ac.kr>
2020-12-04 15:49:49 +0900

    add clip function

--- train_attack_face.py
+++ train_attack_face.py
@@ -2,6 +2,7 @@ import torch
 import advertorch
 import ctypes
 import copy
+import os
 from torch import equal
 from torch.autograd import Variable
 import torch.nn as nn
@@ -14,9 +15,9 @@ from utils.metric import DSSIM
 from model.optimizer import Adadelta, SGD
 from utils.transform import calc_normalize
 
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
-ctypes.cdll.LoadLibrary('caffe2_nvrtc.dll') 
-data_dir = 'data/pubfig65/'
+data_dir = '/hdd_data/pub/pubfig83/dataset/'
 batch_size = 32
 model_path = 'saved/Vgg_face_dag_weight.pt'
 
@@ -50,10 +51,11 @@ class AdvarsarialLoss(nn.Module):
 
         dist_perturb = DSSIM(img, self.source)
         
-        term_perturb = dist_perturb - self.budget
+        term_perturb = self.budget - dist_perturb
         term_perturb = term_perturb ** 2 if term_perturb > 0 else 0
 
         res = term_internal + self.lamb * term_perturb
+        print ("mse %.4f / dis %.4f" % (term_internal, self.lamb * term_perturb))
         return res
 
 
@@ -66,17 +68,15 @@ if __name__=="__main__":
     
     assert (not torch.eq(source_label, target_label))
     result = transforms.ToPILImage()(source_img)
-    result.show()
     result = transforms.ToPILImage()(target_img)
-    result.show()
 
     perturb_img = Variable(source_img.unsqueeze(0).cuda(), requires_grad=True)
     source_img = source_img.unsqueeze(0).cuda()
     target_img = target_img.unsqueeze(0).cuda()
     
     # lambda
-    budget = 0.003
-    lamb = 10 # temporary
+    budget = 0.01
+    lamb = 10000 # temporary
 
     model = VGG_Face_PubFig(saved=True)
 
@@ -85,18 +85,26 @@ if __name__=="__main__":
 
     model.eval()
     model[1].fc8.register_forward_pre_hook(hook)
-    optimizer = SGD([perturb_img], lr=10.0)
+    # optimizer = SGD([perturb_img], lr=10.0)
+    lr = 0.03
+    clip_min = 0.0
+    clip_max = 1.0
     loss_fn = AdvarsarialLoss(model, source_img, target_img, lamb, budget)
 
-    num_epochs = 10
+    num_epochs = 20
     for epoch in range(num_epochs):
         tk_perturb = get_internal_representation(model, perturb_img)
-        optimizer.zero_grad()
+        model.zero_grad()
         loss = loss_fn(perturb_img, tk_perturb)
 
         print('epoch {} : loss {}'.format(epoch, loss))
         loss.backward()
-        optimizer.step()
+
+        # update
+        perturb_img.data = perturb_img.data - lr * perturb_img.grad
+        # clip
+        perturb_img.data = perturb_img.data.clamp(clip_min, clip_max)
+        # optimizer.step()
     
     result = transforms.ToPILImage()(perturb_img.squeeze(0).cpu())
     result.show()
